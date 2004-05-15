e2ee762af447649e3bd6e6d35b1376994bdc3bc8
peter <peter@10f72118-37d7-0310-aa4a-d8d7bed3c02a>
2004-05-15 23:15:02 +0000

    Makefile.*: Added dryerr stuff in dryad.kdev*: Added dryerr in dryerr.[h|cpp]: Split the implementation from the definition, added a namespace

--- dryad/trunk/src/dryerr.cpp
+++ dryad/trunk/src/dryerr.cpp
@@ -1,22 +1,12 @@
-#ifndef _DRYERR_H
-#define _DRYERR_H
+/*
+!!!!!!!!!!!!!
+Again, Tod, put in a copyright notice
+*/
 
-#include <iostream.h>
+#include "dryerr.h"
 
-/*	This file is the error handler for Dryad.
-//	The idea is that you call this function and pass it an
-//	error number.  The Error number gets passed to a switch,
-//	and if an error gets processed, the function returns 1.
-//  Otherwise, it's just a general error and the function
-//	returns 0.  This may be of no use to anyone at this time
-//	but I like to give flexibility where available.  The case
-//	statement will call a script from the scripting file
-//	when we've written the modules to handle them.  Then the
-//	error handling can be customized.  Until then, we'll just
-//	handle the error in a default way, and then we'll keep that
-//	default unless a seperate script has been written.
-//	Feedback to: tod@rtlad.org  */
-
+namespace DError
+{
 
 int dryerr(int errnum)
 {
@@ -91,4 +81,4 @@ int dryerr(int errnum)
 	return diderr;
 }
 
-#endif
+}
